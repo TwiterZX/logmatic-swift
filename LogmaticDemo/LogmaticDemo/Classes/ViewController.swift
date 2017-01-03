bca65acb61381109ba4aacf55297f85f865bfd62
Riad Krim <riad.krim@gmail.com>
2017-01-03 23:19:43 +0100

    Add logmatic library to Demo project Add SendLog button to ViewController, to send a log with a click count as additional info

--- LogmaticDemo/LogmaticDemo/Classes/ViewController.swift
+++ LogmaticDemo/LogmaticDemo/Classes/ViewController.swift
@@ -9,15 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var apiKeyValueLabel: UILabel!
+    @IBOutlet weak var clickCountValueLabel: UILabel!
+    @IBOutlet weak var sendLogButton: UIButton!
+    
+    private var clickCount:Int = 0
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.customizeUI()
+        self.updateClickCount()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func customizeUI() -> Void {
+        if LogmaticLogger.shared.apiKey == nil {
+            self.apiKeyValueLabel.text = "Not Set"
+            self.apiKeyValueLabel.textColor = UIColor.red
+            self.sendLogButton.isEnabled = false
+        }
+        else {
+            self.apiKeyValueLabel.text = LogmaticLogger.shared.apiKey
+            self.apiKeyValueLabel.textColor = UIColor.green
+            self.sendLogButton.isEnabled = true
+        }
     }
 
+    func updateClickCount() -> Void {
+        self.clickCountValueLabel.text = "\(self.clickCount)"
+    }
+
+    @IBAction func sendLog(_ sender: UIButton) {
+        print("sendLog triggered")
+        clickCount += 1
+        self.updateClickCount()
+
+        let logInfo:[String : Int] = ["clickCount" : clickCount]
+        LogmaticLogger.shared.log(dictionary: logInfo, message: "iOS.INFO: Log Action")
+    }
 
 }
